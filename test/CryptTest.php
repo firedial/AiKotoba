94205c37149d03c0b42480121ee04e3ea2463b44
firedial <42674772+firedial@users.noreply.github.com>
2021-12-01 23:14:30 +0900

    base に関するテストを書いた

--- test/CryptTest.php
+++ test/CryptTest.php
@@ -17,6 +17,38 @@ final class CryptTest extends TestCase
         $this->assertSame($password, 'zd?_1TgM6Z1%#H7b');
     }
 
+    public function testBase(): void
+    {
+        $key = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+        $name = 'name';
+        $seed = 'seed';
+        $len = 16;
+
+        $base = 10;
+        $password = Crypt::getPassword($key, $name, $seed, $base, $len);   
+        $this->assertSame($password, '8615002845322849');
+
+        $base = 26;
+        $password = Crypt::getPassword($key, $name, $seed, $base, $len);   
+        $this->assertSame($password, 'dlzsfqvioxhpmjts');
+
+        $base = 36;
+        $password = Crypt::getPassword($key, $name, $seed, $base, $len);   
+        $this->assertSame($password, 'j5ie4cski6o9oozg');
+
+        $base = 52;
+        $password = Crypt::getPassword($key, $name, $seed, $base, $len);   
+        $this->assertSame($password, 'zohhzrWfukEiLVrH');
+
+        $base = 62;
+        $password = Crypt::getPassword($key, $name, $seed, $base, $len);   
+        $this->assertSame($password, 'H083O8qas5SF3HGQ');
+
+        $base = 72;
+        $password = Crypt::getPassword($key, $name, $seed, $base, $len);   
+        $this->assertSame($password, 'zd?_1TgM6Z1%#H7b');
+    }
+
     public function testException(): void
     {
         $key = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
